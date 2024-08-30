9d3e3621588cb37a3f33d598c1cd23963ad40e43
truongduy020406 <votruongduya2@gmail.com>
2024-08-30 17:56:29 +0700

    First commit

---
+++ scr/Blog.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
